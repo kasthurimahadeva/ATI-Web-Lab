5525bd3acd201328b8163dc59f233c49175740e1
mukundan98 <muku1998loga@gmail.com>
2021-09-06 19:37:46 +0530

    Lab3-Answers

---
+++ lab3/palindrome.js
@@ -0,0 +1,30 @@
+function check_palindrome( str )
+{
+    let j = str.length -1;
+    for( let i = 0 ; i < j/2 ;i++)
+    {
+        let x = str[i] ;
+        let y = str[j-i];
+        if( x != y)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+function is_palindrome( str )
+{
+    let ans = check_palindrome(str);
+    if( ans == true )
+    {
+        console.log("passed string is palindrome ");
+    }
+    else
+    {
+        console.log("passed string not a palindrome");
+    }
+}
+let test = "redivider";
+let test1 = "adidas";
+is_palindrome(test);
+is_palindrome(test1);
