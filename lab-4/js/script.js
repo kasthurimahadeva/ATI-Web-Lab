67a08ff49bf69ef12f95deca7fa6f4ee34f2fc25
Sivatharan-STN <sivatharan2019@gmail.com>
2021-09-26 18:02:21 +0530

    script.js created and linked to the existing index.html file, login signup button click functions added

---
+++ lab-4/js/script.js
@@ -0,0 +1,9 @@
+function navigateToLogin(){
+    location.replace("login.html");
+}
+function navigateToSignup(){
+    location.replace("signup.html");
+}
+function navigateToMain(){
+    location.replace("index.html")
+}
