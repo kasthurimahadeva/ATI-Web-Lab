7996c357c16ecccd06f405fbecab51b09e6fdb33
thinushan <rameshthinushan@gmail.com>
2021-09-26 23:53:37 +0530

    lapsheet-4

---
+++ lapsheet-4/login.js
@@ -0,0 +1,19 @@
+function Emptychecker() {
+
+
+
+    let username = document.forms["myform1"]["uname"].value;
+    let Password = document.forms["myform1"]["psw"].value;
+
+
+    if (username== " " || Password == "" ) {
+      alert("empty field");
+      return false;
+    }
+ 
+  
+    else  {
+             alert("complete successfully!");
+           }
+        
+    }
