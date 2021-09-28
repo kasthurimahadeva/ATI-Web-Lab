6011bb8dcc7ad534b73465e40b7cc535601c3ebe
AariyankaPriyanka <sspriyanka1997@gmail.com>
2021-09-28 19:19:26 +0530

    lab4

--- lab-3/Question8.js
+++
@@ -1,9 +0,0 @@
-function chunkArrayInGroups(arr, size) {
-    for (let i = 0; i < arr.length; i++) {
-     let toSplice = arr.splice(i, size);
-     arr.unshift(toSplice);
-   }
-   return arr.reverse();;
- }
- 
- chunkArrayInGroups(["a", "b", "c", "d"], 2);
